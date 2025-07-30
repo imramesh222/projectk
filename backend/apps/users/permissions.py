1520e41b18c6d4f1de01a5b6ba2b99486f598471
Ramesh Rawat <imrameshrawat@gmail.com>
2025-07-31 01:36:11 +0545

    changes nade to permissions

--- backend/apps/users/permissions.py
+++ backend/apps/users/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticated
+
 
 class BaseRolePermission(permissions.BasePermission):
     """Base permission class for role-based access control."""
